b1f40554e1983cf83a4338919bfab422f8cf9496
JashanDhiman <jashandhiman.hulk@gmail.com>
2022-09-05 22:35:24 +0530

    connect the mongodb for storing signin user data

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { getSession } from "next-auth/react";
 
-const about = () => {
-  return <div>welcome to about page</div>;
+const About = ({ data }) => {
+  return <div>welcome to about page - {data}</div>;
 };
 
-export default about;
+export default About;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/signin?callbackUrl=http://localhost:3000/about",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+      data: session ? "user" : "Guest",
+    },
+  };
+}
